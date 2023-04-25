9ae73ea867aec131b91684ee175b52b3042de29e
visteen <visteen192@gmail.com>
2023-04-25 14:21:17 +0000

    import error fixed

--- base/seraliazers.py
+++ base/seraliazers.py
@@ -9,33 +9,35 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = '__all__'
         
 class UserSerializer(serializers.ModelSerializer):
-    _id = serializers.SerializerMethodField(read_only=True)
     name = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
     isAdmin = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = User
-        fields = ['_id','username','email','name','isAdmin']
-        
-        
-    def get__id(self,obj):
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin']
+
+    def get__id(self, obj):
         return obj.id
-        
-    def get_isAdmin(self,obj):
+
+    def get_isAdmin(self, obj):
         return obj.is_staff
-        
-    def get_name(self,obj):
+
+    def get_name(self, obj):
         name = obj.first_name
         if name == '':
             name = obj.email
+
         return name
-        
+
+
 class UserSerializerWithToken(UserSerializer):
     token = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = User
-        fields = ['_id','username','email','name','isAdmin','token']
+        fields = ['_id', 'username', 'email', 'name', 'isAdmin', 'token']
 
-    def get_token(self,obj):
-        token = RefreshToken().for_user(obj)
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
         return str(token.access_token)
